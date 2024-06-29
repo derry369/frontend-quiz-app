03ee71ee14e56c01fbad66bb92fceb0423bc7ede
Derrick Danso <derrickdansodd3@gmail.com>
2024-06-29 19:16:35 +0000

    font setup

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+:root {
+
+/* Set core body defaults */
+body {
+  min-height: 100vh;
+  line-height: 1.5;
+  font-family: "Rubik", sans-serif;
+
+
+}
+
+}
+
+`;
+export default GlobalStyles;
