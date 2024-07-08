d3abbcab1731df29e141f99498ed59719c7c860a
Derrick Danso <derrickdansodd3@gmail.com>
2024-07-08 23:54:39 +0000

    route to quiz

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import QuizStart from "../components/QuizStart";
-//import QuizQuestionsAnswers from "../components/QuizQuestionsAnswers";
+import QuizQuestionsAnswers from "../components/QuizQuestionsAnswers";
 //import ResultsPage from "../components/ResultsPage";
 
 const router = createBrowserRouter([
@@ -8,11 +8,11 @@ const router = createBrowserRouter([
     path: "/",
     element: <QuizStart />,
   },
-  /*{
+  {
     path: "quiz/:type/question/:id",
     element: <QuizQuestionsAnswers />,
   },
-
+/*
   {
     path: "quiz/:type/results",
     element: <ResultsPage />,
