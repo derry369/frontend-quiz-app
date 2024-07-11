4f20e3b444db20e6fb982433978c2d61dc3071fd
Derrick Danso <derrickdansodd3@gmail.com>
2024-07-11 10:51:23 +0000

    fix broken image links

--- src/components/ConMain.tsx
+++ src/components/ConMain.tsx
@@ -13,8 +13,8 @@ const StyledConMain = styled.div<StyledConMainProps>`
   background: ${(props) =>
     `var(--cl-bg-main) ${
       props.$isDarkMode
-        ? "url('images/pattern-background-mobile-dark.svg')"
-        : "url('images/pattern-background-mobile-light.svg')"
+        ? "url('/images/pattern-background-mobile-dark.svg')"
+        : "url('/images/pattern-background-mobile-light.svg')"
     } no-repeat center`};
 
   ${flex}
@@ -24,16 +24,16 @@ const StyledConMain = styled.div<StyledConMainProps>`
     background: ${(props) =>
       `var(--cl-bg-main) ${
         props.$isDarkMode
-          ? "url('images/pattern-background-tablet-dark.svg')"
-          : "url('images/pattern-background-tablet-light.svg')"
+          ? "url('/images/pattern-background-tablet-dark.svg')"
+          : "url('/images/pattern-background-tablet-light.svg')"
       } no-repeat left top`};
   }
   @media (min-width: 1024px) {
     background: ${(props) =>
       `var(--cl-bg-main) ${
         props.$isDarkMode
-          ? "url('images/pattern-background-desktop-dark.svg')"
-          : "url('images/pattern-background-desktop-light.svg')"
+          ? "url('/images/pattern-background-desktop-dark.svg')"
+          : "url('/images/pattern-background-desktop-light.svg')"
       } no-repeat left top`};
     background-size: cover;
   }
